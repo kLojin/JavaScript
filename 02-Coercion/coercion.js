8d928e61893bc66c732b50303ac99afc279c1c99
Jon Mikhail Garcia <138591617+kLojin@users.noreply.github.com>
2023-08-17 21:22:18 +0800

    updated

--- 02-Coercion/coercion.js
+++ 02-Coercion/coercion.js
@@ -34,17 +34,46 @@
 // Convert number to a string
 // Convert object to a boolean
 
-let a = 10;
-console.log(typeof a); // First console.log a datatype of a number
+// let a = 10;
+// console.log(typeof a); // First console.log a datatype of a number
 
-let b = '7';
-console.log(typeof b); // Second console.log a datatype of a string
-b = Number(b);
-console.log(b);
-console.log(typeof b); // Third conversion of b from a string to a number
+// let b = '7';
+// console.log(typeof b); // Second console.log a datatype of a string
+// b = Number(b);
+// console.log(b);
+// console.log(typeof b); // Third conversion of b from a string to a number
 
 // TYPE CONVERSIONS: EXPLICIT AND IMPLICIT
 // IMPLICIT TYPE CONVERSION = COERCION (automatically done during code execution)
 // EXPLICIT TYPE CONVERSION = TYPE CASTING (done by you the developer)
 
 // Type coercion is implicit whereas type conversion can be either implicit or explicit.
+
+function explicit(){
+  let d = 5;
+  let e = '6';
+  e = parseInt(e, 10);
+  let f = d + e;
+  console.log("Explicit Answer: " + f);
+  console.log(typeof f);
+}
+
+function implicit() {
+  if (1 === "1") {
+    let a = 1; // numbere
+    let b = "1"; // string
+    let c = a + b;
+    console.log("Implicit Answer: " + c);
+    console.log(typeof c);
+    document.getElementById("true").innerHTML ="This executed properly"
+  } else {
+    document.getElementById("false").innerHTML =
+      "This did not execute because of the strict equality operator.";
+    console.log(
+      "This did not execute because of the strict equality operator."
+    );
+  }
+}
+
+// explicit();
+// implicit();
